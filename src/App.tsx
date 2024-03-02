9dc23f61b1d39d42dd0d970b855c3051dea67a17
RayAra2004 <raynan.araujo@gmail.com>
2024-03-02 14:30:09 -0300

    feat: employee survey

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,47 @@ import api from './services/api';
 
 function App() {
   const [employees, setEmployees] = useState<Employee[] | undefined>(undefined);
+  const [employeesSurveyed, setEmployeesSurveyed] = useState<
+    Employee[] | undefined
+  >(undefined);
+  const [textInput, setTextInput] = useState<string>('');
 
   useEffect(() => {
     api
       .get('/employees')
       .then(response => {
         setEmployees(response.data);
+        setEmployeesSurveyed(response.data);
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    const searchResult = employees?.filter(employee => {
+      const searchLowerCase = textInput?.toLowerCase().trim();
+      return (
+        employee.name.toLowerCase().includes(searchLowerCase) ||
+        employee.job.toLowerCase().includes(searchLowerCase) ||
+        employee.phone.toLowerCase().includes(searchLowerCase)
+      );
+    });
+
+    setEmployeesSurveyed(searchResult);
+  }, [textInput]);
   return (
     <SCApp>
       <Header />
       <main>
         <SCDivSearch>
           <h1>Funcionários</h1>
-          <SearchBar />
+          <SearchBar textInput={textInput} setTextInput={setTextInput} />
         </SCDivSearch>
         <table>
           <TableHeader />
           <tbody>
-            {employees?.map(employee => (
+            {employeesSurveyed?.map(employee => (
               <TableRow key={employee.id} {...employee} />
             ))}
           </tbody>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,22 @@
 import Search from '../assets/icons/Search';
 import { SCSearchBar } from '../style/SearchBar/SearchBar';
 
-export default function SearchBar() {
+type SearchBarProps = {
+  textInput: string;
+  setTextInput: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function SearchBar(props: SearchBarProps) {
+  const { textInput, setTextInput } = props;
+
   return (
     <SCSearchBar>
-      <input type="text" placeholder="Pesquisar" />
+      <input
+        type="text"
+        placeholder="Pesquisar"
+        value={textInput}
+        onChange={e => setTextInput(e.target.value)}
+      />
       <Search />
     </SCSearchBar>
   );
